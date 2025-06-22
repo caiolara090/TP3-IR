c8ef8e63179c508294907751eac2d7a19fa24e70
lucasaamaral <lucas.almeida1048@gmail.com>
2025-06-22 00:30:12 -0300

    Ajuste na pipeline

--- query_processor2.py
+++ query_processor2.py
@@ -2,13 +2,11 @@ import os
 import csv
 import re
 import logging
-import lightgbm as lgb
 import pandas as pd
 import pyterrier as pt
 from tqdm import tqdm
 import datetime
 from pyterrier_t5 import MonoT5ReRanker
-from sklearn.model_selection import train_test_split
 
 pt.java.init()
 
@@ -45,12 +43,6 @@ def run_pipeline():
     qrels["qid"] = qrels["qid"].astype(str)
     qrels["docno"] = qrels["docno"].astype(str)
 
-    train_queries_train, train_queries_val = train_test_split(
-        train_queries, test_size=0.2, random_state=42
-    )
-    train_qrels_train = qrels[qrels["qid"].isin(train_queries_train["qid"])]
-    train_qrels_val = qrels[qrels["qid"].isin(train_queries_val["qid"])]
-
     bm25 = pt.terrier.Retriever(
         index,
         wmodel="BM25F",
@@ -59,69 +51,14 @@ def run_pipeline():
         verbose=True,
     )
 
-    # pipeline = (
-    #     bm25
-    #     >> pt.text.get_text(index, ["title", "text", "keywords"])
-    #     >> MonoT5ReRanker()
-    # )
-
-    # logging.info("Avaliando desempenho no conjunto de treino...")
-    # results = pipeline.transform(train_queries)
-    # eval_metrics = pt.Utils.evaluate(
-    #     results, qrels, metrics=["map", "ndcg", "recall", "P.10"]
-    # )
-    # for metric, value in eval_metrics.items():
-    #     logging.info(f"{metric}: {value:.4f}")
-
-    # test_queries = pd.read_csv(TEST_QUERIES_PATH)
-    # test_queries["Query"] = test_queries["Query"].apply(preprocess_text)
-    # test_queries = test_queries.rename(columns={"QueryId": "qid", "Query": "query"})
-    # test_queries["qid"] = test_queries["qid"].astype(str)
-
-    # logging.info("Executando pipeline no conjunto de teste...")
-    # test_results = pipeline.transform(test_queries)
-    # test_results = test_results.groupby("qid").head(100)
-
-    tfidf = pt.terrier.Retriever(index, wmodel="TF_IDF", num_results=HITS_PER_QUERY)
-    pl2 = pt.terrier.Retriever(index, wmodel="PL2", num_results=HITS_PER_QUERY)
-    rerank_feats = tfidf**pl2**bm25
-    ltr_feats_pipeline = bm25 >> rerank_feats
-
-    print("Pipeline de recursos LTR:")
-
-    print(ltr_feats_pipeline)
-
-    lmart_l = lgb.LGBMRanker(
-        task="train",
-        min_data_in_leaf=10,
-        min_sum_hessian_in_leaf=1,
-        max_bin=511,
-        num_leaves=31,
-        max_depth=20,
-        objective="lambdarank",
-        metric="ndcg",
-        ndcg_eval_at=[1, 3, 5, 10],
-        learning_rate=0.1,
-        importance_type="gain",
-        num_iterations=5000,
-        subsample=0.8,
-        colsample_bytree=0.8,
-        reg_alpha=0.1,
-        reg_lambda=1.0,
-        random_state=42,
-        verbose=-1,
-        n_jobs=6,
-    )
-
-    lmart_l_pipe = ltr_feats_pipeline >> pt.ltr.apply_learned_model(lmart_l, form="ltr")
-
-    logging.info("Treinando modelo LambdaMART com LightGBM...")
-    lmart_l_pipe.fit(
-        train_queries_train, train_qrels_train, train_queries_val, train_qrels_val
+    pipeline = (
+        bm25
+        >> pt.text.get_text(index, ["title", "text", "keywords"])
+        >> MonoT5ReRanker()
     )
 
     logging.info("Avaliando desempenho no conjunto de treino...")
-    results = lmart_l_pipe.transform(train_queries)
+    results = pipeline.transform(train_queries)
     eval_metrics = pt.Utils.evaluate(
         results, qrels, metrics=["map", "ndcg", "recall", "P.10"]
     )
@@ -134,7 +71,7 @@ def run_pipeline():
     test_queries["qid"] = test_queries["qid"].astype(str)
 
     logging.info("Executando pipeline no conjunto de teste...")
-    test_results = lmart_l_pipe.transform(test_queries)
+    test_results = pipeline.transform(test_queries)
     test_results = test_results.groupby("qid").head(100)
 
     os.makedirs(os.path.dirname(SUBMISSION_OUTPUT_PATH), exist_ok=True)
