efa7c31a6ad1007e863e70215224cd44c620626a
lucasaamaral <lucas.almeida1048@gmail.com>
2025-06-22 21:42:46 -0300

    Renomeando arquivos

--- neural.py
+++ neural.py
@@ -17,8 +17,6 @@ now_str = datetime.datetime.now().strftime("%m_%d_%H.%M")
 SUBMISSION_OUTPUT_PATH = f"results/kaggle_submission_{now_str}.csv"
 
 INDEX_DIR = os.path.abspath("indexes/terrier")
-TRAIN_QUERIES_PATH = "data/train_queries.csv"
-TRAIN_QRELS_PATH = "data/train_qrels.csv"
 TEST_QUERIES_PATH = "data/test_queries.csv"
 HITS_PER_QUERY = 1000
 
@@ -35,18 +33,6 @@ def concat_fields(df):
 def run_pipeline():
     index = pt.IndexFactory.of(INDEX_DIR)
 
-    train_queries = pd.read_csv(TRAIN_QUERIES_PATH)
-    train_queries["Query"] = train_queries["Query"]
-    train_queries = train_queries.rename(columns={"QueryId": "qid", "Query": "query"})
-    train_queries["qid"] = train_queries["qid"].astype(str)
-
-    qrels = pd.read_csv(TRAIN_QRELS_PATH)
-    qrels = qrels.rename(
-        columns={"QueryId": "qid", "EntityId": "docno", "Relevance": "label"}
-    )
-    qrels["qid"] = qrels["qid"].astype(str)
-    qrels["docno"] = qrels["docno"].astype(str)
-
     bm25f = pt.terrier.Retriever(
         index,
         wmodel="BM25F",
@@ -61,14 +47,6 @@ def run_pipeline():
         >> pt.apply.generic(concat_fields)
         >> MonoT5ReRanker(text_field='doc_text')
     )
-
-    logging.info("Avaliando desempenho no conjunto de treino...")
-    results = pipeline.transform(train_queries)
-    eval_metrics = pt.Utils.evaluate(
-        results, qrels, metrics=["map", "ndcg", "recall", "P.10"]
-    )
-    for metric, value in eval_metrics.items():
-        logging.info(f"{metric}: {value:.4f}")
 
     test_queries = pd.read_csv(TEST_QUERIES_PATH)
     test_queries["Query"] = test_queries["Query"]
